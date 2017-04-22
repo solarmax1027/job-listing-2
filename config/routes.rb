225377d8c69c76f67c1a1fe286e34324b2011764
solarmax <solarmax1027@gmail.com>
2017-04-22 18:41:04 +0800

    better welcome/index & admin/jobs/index

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
         post :hide
       end
 
-      resources :resumes 
+      resources :resumes
     end
   end
 
@@ -19,5 +19,5 @@ Rails.application.routes.draw do
     resources :resumes
   end
 
-  root 'jobs#index'
+  root 'welcome#index'
 end
